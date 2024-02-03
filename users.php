49e9607d852a1c7efd80ad898689c75141da5cb0
erionashahini <erionashahini10@gmail.com>
2024-02-03 18:43:46 +0100

    users.php

---
+++ users.php
@@ -0,0 +1,30 @@
+<?php
+  $user1 = [
+    "name"=>"Erion",
+    "surname"=>"Shahini",
+    "email"=>"es@ubt-uni.net",
+    "username"=>"erionshahini",
+    "password" => "erioni123",
+    "role"=>"user"
+  ];
+  
+  $user2 = [
+    "name"=>"Eriona",
+    "surname"=>"Shahini",
+    "email"=>"es@ubt-uni.net",
+    "username"=>"erionashahini",
+    "password" => "eriona123",
+    "role"=>"user"
+  ];
+
+  $user3 = [
+    "name"=>"Drilon",
+    "surname"=>"Gashi",
+    "email"=>"dg@ubt-uni.net",
+    "username"=>"drilongashi",
+    "password" => "driloni123",
+    "role"=>"user"
+  ];
+
+  $users = [$user1, $user2, $user3];
+?>
